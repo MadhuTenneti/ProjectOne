933e03e66ccac8dce097cefd6cc4882a7df9c4cb
unknown <mr.madhutenneti109@gmail.com>
2024-05-15 22:33:29 +0530

    commiting files firstly

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Footer from "../Footer";
+import Header from "../Header";
+
+function HomePage(){
+    return(
+        <>
+        <Header/>
+        <Footer/>
+        </>
+    )
+}
+export default HomePage;
