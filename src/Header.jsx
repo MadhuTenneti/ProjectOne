50650d3e625f49b8d86e76b1bfb99983fa1dd10a
unknown <mr.madhutenneti109@gmail.com>
2024-05-20 12:08:21 +0530

    changes to master

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,8 @@
-function Header(){
-    return(
-        <>
-        <h1 className="text-center mt-5">Header</h1>
-        </>
-    )
+function Header() {
+  return (
+    <>
+      <h1 className="text-center mt-5">Header</h1>
+    </>
+  );
 }
 export default Header;
